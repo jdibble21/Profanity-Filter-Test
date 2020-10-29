349e45a077d03b078986619aa8dbbdea0f93b97e
Jake Dibble <jdibble@bsu.edu>
2020-10-29 18:01:49 -0400

    Add documentation comments

--- filter_keyword_test.php
+++ filter_keyword_test.php
@@ -1,13 +1,13 @@
 <?php
 // Separate file from index.php for testing and experimenting
 
+// When running just this file, change $input to test custom input
 $input = "jake is a nice shit";
 $input_array = explode(" ",$input);
 
 $is_bad = False;
 $file_contents = file_get_contents("blacklist.txt");
 $blacklist_array = array(explode('/',$file_contents));
-
 for ($j=0; $j < count($input_array); $j++){
     for ($i=0; $i < count($blacklist_array[0]); $i++){
         if($input_array[$j] == strval($blacklist_array[0][$i])){
@@ -15,9 +15,10 @@ for ($j=0; $j < count($input_array); $j++){
         }
     }
 }
-
+// When we integrate with the Betterflye system, this will come up as an BLOCKED/ALLOWED
+// admin message and pop up message for the user, not just a print statement
 if($is_bad){
-    print_r("<h4>BLOCKED</h4>");
+    print_r("BLOCKED");
 }else{
-    print_r("<h4>ALLOWED</h4>");
+    print_r("ALLOWED");
 }
